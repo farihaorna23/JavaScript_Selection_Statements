cf816ee3f32e29dd10b551c54841e68f71d1c564
farihaorna23 <farihaorna@gmail.com>
2022-08-15 15:49:46 -0400

    Finished exercise1

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+
+//Exercise1
+const favoriteNumber = 23;
+
+const guess = Number(prompt("Guess my favorite number"));
+console.log(typeof guess);
+
+if (guess < favoriteNumber) {
+  console.log("too low");
+} else if (guess > favoriteNumber) {
+  console.log("too high");
+} else {
+  console.log("Congratulations!!!");
+}
